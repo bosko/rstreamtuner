96807013aa58468031a18ec791f63e79acb3582e
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2009-12-26 20:01:27 +0100

    Add script path to load path

--- rstreamtuner.rb
+++ rstreamtuner.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 
 __DIR__ = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift __DIR__
 $LOAD_PATH.unshift File.join(__DIR__, 'streams')
 $LOAD_PATH.unshift File.join(__DIR__, 'tk')
 
