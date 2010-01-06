c274237a26206870a06c3f0be9d853ad795a71fb
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2010-01-06 14:15:18 +0100

    Application configuration

---
+++ rst_config.rb
@@ -0,0 +1,34 @@
+module RstConfig
+  # Allows accessing config variables from rst.yml like so:
+  # RstConfig[:domain] => some-domain.com
+  def self.[](key)
+    unless @config
+      @config = YAML.load(config_file) if File.exist?(config_file)
+    end
+    @config[key]
+  end
+  
+  def self.[]=(key, value)
+    @config[key.to_sym] = value
+  end
+
+  def self.save_config
+    if @config
+      File.open(config_file, 'w') do |f|
+        f.write @config.to_yaml
+      end
+    end
+  end
+  
+  def self.config_dir
+    dir = File.join(ENV['home'], '.rstreamtuner')
+    unless File.exist?(dir)
+      FileUtils.mkdir_p(dir)
+    end
+    dir
+  end
+
+  def self.config_file
+    File.read(File.join(config_dir, "rst.yml"))
+  end
+end
