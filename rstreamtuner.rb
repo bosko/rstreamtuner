4dac7af41c2f7dae157345687d83aeaa87a05457
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2010-01-06 13:38:47 +0100

    Remove Tk version & define App in main file

--- rstreamtuner.rb
+++ rstreamtuner.rb
@@ -1,27 +1,23 @@
-require 'rubygems'
-
 __DIR__ = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift __DIR__
 $LOAD_PATH.unshift File.join(__DIR__, 'streams')
-$LOAD_PATH.unshift File.join(__DIR__, 'tk')
 $LOAD_PATH.unshift File.join(__DIR__, 'wx')
 
+require 'rubygems'
 require 'stream_api'
-require 'app'
-#require 'rstreamer_gui'
+require 'wx'
+require 'main_frame'
 
 Dir.glob("#{__DIR__}/streams/*.rb") do |s|
   require File.expand_path(s)
 end
 
+class RStreamTunerApp < Wx::App
+  def on_init
+    f = MainFrame.new
+    f.show(true)
+  end
+end
+
 app = RStreamTunerApp.new
 app.main_loop
-# gui = RStreamerGui.new("Ruby Stream Tuner")
-# gui.start
-# shoutcast = ShoutcastStream.new
-# shoutcast.search!('love')
-# #shoutcast.fetch!
-# File.open('shoutcast.yml', 'w') do |f|
-#   f.write shoutcast.stations.to_yaml
-# end
-
