840f4d8b9bd30ceaf48b7c79e059b57624a1c1e9
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2010-02-14 21:05:01 +0100

    Station list sorting

--- wx/stations_list.rb
+++ wx/stations_list.rb
@@ -1,8 +1,11 @@
 class StationsList < Wx::ListCtrl
   attr_accessor :stations
+
+  ASCENDING = -1
+  DESCENDING = 1
   
   def initialize(parent, id)
-    super(parent, id, :style => Wx::LC_REPORT | Wx::LC_VIRTUAL)
+    super(parent, id, :style => Wx::LC_REPORT | Wx::LC_VIRTUAL | Wx::LC_VRULES | Wx::LC_SINGLE_SEL)
     @even_attr = Wx::ListItemAttr.new
     @even_attr.set_background_colour(Wx::Colour.new('LIGHT BLUE'))
   end
@@ -13,13 +16,26 @@ class StationsList < Wx::ListCtrl
     @columns = columns
     @columns.each_with_index do |col, idx|
       insert_column(idx, col[:header])
+      evt_list_col_click(id) do |event|
+        case @sort_order
+        when ASCENDING
+          @stations.reverse!
+          @sort_order = DESCENDING
+        else
+          @stations.sort! do |a, b|
+            a[@columns[event.get_column][:attr]] <=> b[@columns[event.get_column][:attr]]
+          end
+          @sort_order = ASCENDING
+        end
+      end
+      
       set_column_width(idx, col[:width]) if col[:width]
     end
   end
   
   def on_get_item_text(item, col)
     text = ''
-    if @stations and col < @stations.length
+    if @stations and item < @stations.length
       text = @stations[item][@columns[col][:attr]]
     end
 
